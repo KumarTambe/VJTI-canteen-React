7915a591c7d474fffa34f1837109b3a62d8a604c
Kumar Tambe <kumartambe49@gmail.com>
2026-06-27 13:44:10 +0530

    created login fucntionality

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,28 @@
+import { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { AuthContext } from "../context/AuthContext";
+
 function Login() {
+    const { user, setUser, isLoggedIn, setIsLoggedIn, isAdmin, setIsAdmin } = useContext(AuthContext);
+    const [input, setInput] = useState('');
+    const navigate = useNavigate();
+
+    function handleLogIn() {
+        setIsLoggedIn(true);
+        setUser(input);
+        navigate('/dashboard');
+    }
+
     return (
-        <h1>Login page</h1>
+        <>
+            <h1>Login page</h1>
+            <input
+                type="text"
+                onChange={(e) => setInput(e.target.value)}
+            />
+            <button onClick={handleLogIn}>Login</button>
+        </>
     )
 }
 
