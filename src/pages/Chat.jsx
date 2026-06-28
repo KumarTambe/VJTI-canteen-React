09c60918249c01ddba66a3472ce4e50112b305b0
Kumar Tambe <kumartambe49@gmail.com>
2026-06-29 00:08:46 +0530

    added chat functinonality

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,6 +1,34 @@
+import { useContext, useState } from "react";
+import { AuthContext } from "../context/AuthContext";
+
 function Chat() {
+    const [savedMessages, setSavedMessages] = useState([
+        { id: 1, user: "XYZ", text: "Hello World" }
+    ])
+    const { user, setUser } = useContext(AuthContext)
+    const [msg, setMsg] = useState('');
+
+    function handleSubmit() {
+        setSavedMessages([...savedMessages, { id: Date.now(), user: user, text: msg }])
+        setMsg('');
+    }
     return (
-        <h1> Chat page</h1>
+        <>
+            <h1> Chat page</h1>
+            <ul>
+                {savedMessages.map((message) => (
+                    <li key={message.id}>
+                        <strong>{message.user}</strong>{message.text}
+                    </li>
+                ))}
+            </ul>
+            <input
+                type="text"
+                value={msg}
+                onChange={(e) => setMsg(e.target.value)}
+            />
+            <button onClick={handleSubmit}>Submit</button>
+        </>
     )
 }
 
