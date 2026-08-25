5c5e6eb6ccc8dab92c19dd108575c1288d13142b
Kumar Tambe <kumartambe49@gmail.com>
2026-08-26 00:59:42 +0530

    fixed authContext

--- Frontend/src/context/AuthContext.jsx
+++ Frontend/src/context/AuthContext.jsx
@@ -1,21 +1,17 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState } from "react";
 
 export const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
     const [user, setUser] = useState(null);
     const [isLoggedIn, setIsLoggedIn] = useState(() => {
-        return localStorage.getItem('isLoggedIn') === 'true'
+        return !!localStorage.getItem('token')
     })
     const [isAdmin, setIsAdmin] = useState(false);
 
-    useEffect(() => {
-        localStorage.setItem('isLoggedIn', isLoggedIn)
-    }, [isLoggedIn])
     return (
         <AuthContext.Provider value={{ user, setUser, isLoggedIn, setIsLoggedIn, isAdmin, setIsAdmin }}>
             {children}
         </AuthContext.Provider>
     )
 }
-
